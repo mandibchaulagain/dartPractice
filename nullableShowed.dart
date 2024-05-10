3f21de55bfbdd6d97c82611ae3c927737f509d7a
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-10 20:09:36 +0545

    used String? cast to allow second parameter to not come

---
+++ nullableShowed.dart
@@ -0,0 +1,8 @@
+void main() {
+  display("Ram", "Hero");
+  display("Sita");
+}
+
+void display(String name, [String? cast]) {
+  print("Congratulations $name, you have obtained the $cast role.");
+}
