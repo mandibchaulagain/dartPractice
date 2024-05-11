7e1908cac62eaf5dfd8a673027daf5aa5b6a9e07
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-11 20:35:28 +0545

    showed how parameter values interact

--- nullableShowed.dart
+++ nullableShowed.dart
@@ -1,8 +1,14 @@
 void main() {
   display("Ram", "Hero");
-  display("Sita");
+  display2("Sita");
+  display("Ganesh", null);
+  display2("Laxman", "Villain");
 }
 
 void display(String name, [String? cast]) {
   print("Congratulations $name, you have obtained the $cast role.");
+}
+
+void display2(String name, [String? cast = ""]) {
+  print("Hello. $name $cast");
 }
